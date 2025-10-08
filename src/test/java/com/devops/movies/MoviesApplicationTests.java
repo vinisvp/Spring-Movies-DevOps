d7e4766f65b0cd11fc2f8aa9f871e93804eca884
vinisvp <viniciuspedro0905@gmail.com>
2025-10-08 17:45:10 -0300

    Initial Commit

---
+++ src/test/java/com/devops/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
