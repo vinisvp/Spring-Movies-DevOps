8a8ef024d3b068c9ee66244f2691e87d568341b0
JOÃO VITOR DE PAULA OLIVEIRA <1781432412008@fatecitu.local>
2025-10-20 16:45:56 -0300

    endpoint cliente

--- src/main/java/com/devops/movies/controller/CustomerController.java
+++ src/main/java/com/devops/movies/controller/CustomerController.java
@@ -12,19 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    
+
     @Autowired
     private CustomerService customerService;
-    
+
     @PostMapping
     public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO requestDTO) {
-        CustomerResponseDTO response = customerService.createCustomer(requestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        try {
+            CustomerResponseDTO response = customerService.createCustomer(requestDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
-    
+
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable Integer id, @Valid @RequestBody CustomerRequestDTO requestDTO) {
-        CustomerResponseDTO response = customerService.updateCustomer(id, requestDTO);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable Integer id,
+            @Valid @RequestBody CustomerRequestDTO requestDTO) {
+        try {
+            CustomerResponseDTO response = customerService.updateCustomer(id, requestDTO);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
