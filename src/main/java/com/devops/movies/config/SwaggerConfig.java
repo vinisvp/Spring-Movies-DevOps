8d6c8f77002c5d267ccc2e35492246c5bb2a96e4
vinisvp <viniciuspedro0905@gmail.com>
2025-11-26 17:48:31 -0300

    Manage authorization on Swagger is possible

--- src/main/java/com/devops/movies/config/SwaggerConfig.java
+++ src/main/java/com/devops/movies/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.devops.movies.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Movies API")
                         .version("1.0")
-                        .description("API para gerenciamento de filmes"));
+                        .description("API para gerenciamento de filmes"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
